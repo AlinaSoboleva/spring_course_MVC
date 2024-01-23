1eede86b0a39562c495052b6d4d5aeccfebd1f02
Alina <sobolevaalinaa@yandex.ru>
2024-01-23 15:31:17 +0300

    create method showAllEmployees();

--- spring_course_rest/src/main/java/com/alina/spring/rest/controller/MyRestController.java
+++ spring_course_rest/src/main/java/com/alina/spring/rest/controller/MyRestController.java
@@ -1,7 +1,27 @@
 package com.alina.spring.rest.controller;
 
+import com.alina.spring.rest.entity.Employee;
+import com.alina.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class MyRestController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public MyRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employee")
+    public List<Employee> showAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
 }
