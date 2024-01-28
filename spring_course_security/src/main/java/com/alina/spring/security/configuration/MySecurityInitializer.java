d73c4dc7563a7ee64769a8c92721d39f02fdb92e
Alina <sobolevaalinaa@yandex.ru>
2024-01-28 17:01:20 +0300

    add security config

---
+++ spring_course_security/src/main/java/com/alina/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.alina.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//Что бы выходило окно для ввода логина и пароля
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
