29fdc842972a2eef48dcef118ad8ef582a8c3d9c
Alina <sobolevaalinaa@yandex.ru>
2024-01-31 14:30:13 +0300

    password in db

--- spring_course_security/src/main/java/com/alina/spring/security/configuration/MySecurityConfig.java
+++ spring_course_security/src/main/java/com/alina/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.alina.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,20 +8,30 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //используем дефолтное шифрование паролей
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("alina").password("alina").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
-                .withUser(userBuilder.username("katya").password("katya").roles("MANAGER", "HR"));
+        //используем дефолтное шифрование паролей (inMemory)
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("alina").password("alina").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
+//                .withUser(userBuilder.username("katya").password("katya").roles("MANAGER", "HR"));
+
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.authorizeRequests((authorizeRequest) ->
                 {
                     try {
